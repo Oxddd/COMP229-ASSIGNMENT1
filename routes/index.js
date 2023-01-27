64f31a3bfe67e6658517e27a1d753208dac2c66c
Oxddd <xiangdeou@gmail.com>
2023-01-27 17:36:25 -0500

    ++ dependience

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', 
-  { title: 'Express' ,
-    mainheading:'Main Heading'
+  res.render('index', { title: 'Home' });
 });
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
 });
 
 module.exports = router;
